5269fa16db202e005f5ba66e3a084cb89220977b
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-19 13:49:04 +0200

    XML

---
+++ XmlClient/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ModelLib;
+
+namespace XmlClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            ModelLib.Car testCar = new Car();
+           
+            testCar.Model = "Tesla";
+            testCar.Color = "blue";
+            testCar.RegNo = "XMLCar23";
+
+
+
+            using (TcpClient socket = new TcpClient("localhost", 10002))
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+
+                XmlSerializer carSerializer = new XmlSerializer(typeof(Car));
+                carSerializer.Serialize(sw, testCar);
+                sw.Flush();
+
+                Console.WriteLine("Sender: "+testCar);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XmlServer/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLib;
+using System.Xml.Serialization;
+
+namespace XmlServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 10002);
+            server.Start();
+
+            using (TcpClient socket = server.AcceptTcpClient())
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                XmlSerializer carSerializer = new XmlSerializer(typeof(Car));
+                Car carCopy = (Car) carSerializer.Deserialize(sr);
+                Console.WriteLine("carCopy: " + carCopy);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
